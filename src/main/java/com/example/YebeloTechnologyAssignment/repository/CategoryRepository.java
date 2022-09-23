cc5d7fd49ab45a752f287755573ba8c7304684e3
Anupal00 <patra.anupal@outlook.com>
2022-09-23 10:46:29 +0530

    creating find and update method into repository

--- src/main/java/com/example/YebeloTechnologyAssignment/repository/CategoryRepository.java
+++ src/main/java/com/example/YebeloTechnologyAssignment/repository/CategoryRepository.java
@@ -2,8 +2,17 @@ package com.example.YebeloTechnologyAssignment.repository;
 
 import com.example.YebeloTechnologyAssignment.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category,Long> {
+
+    Category findByCategoryCode(String categoryCode);
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    @Query(value = "update category set value = ?1 where category_code = ?2",nativeQuery = true)
+    int updateByValue(Long value,String categoryCode);
 }
