f3acf709f41b630f3756d0773363a1cff18f5ce0
Anupal00 <patra.anupal@outlook.com>
2022-09-23 11:22:58 +0530

    creating two methods update and fetchNextNumber in service

--- src/main/java/com/example/YebeloTechnologyAssignment/service/CategoryService.java
+++ src/main/java/com/example/YebeloTechnologyAssignment/service/CategoryService.java
@@ -6,18 +6,51 @@ import com.example.YebeloTechnologyAssignment.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CategoryService {
 
     @Autowired
     private CategoryRepository categoryRepository;
 
-    public Category findByCategoryCode(DemoCategoryCode demoCategoryCode){
-        return categoryRepository.findByCategoryCode(demoCategoryCode.getCategoryCode());
+    public Category findByCategoryCode(String categoryCode){
+        return categoryRepository.findByCategoryCode(categoryCode);
     }
 
     public Category save(Category c){
         return categoryRepository.save(c);
+    }
+
+    public Category updateByValue(Category category){
+        Category c = categoryRepository.findByCategoryCode(category.getCategoryCode());
+        Long value = c.getValue();
+        long sumOfDigit;
+        while(value > 10) {
+            sumOfDigit = 0L;
+            while (value != 0) {
+                long temp = value % 10;
+                sumOfDigit += temp;
+                value = value / 10;
+            }
+            value = sumOfDigit;
+        }
+        long nextValue = c.getValue() + (10-value);
+        categoryRepository.updateByValue(nextValue,category.getCategoryCode());
+        return categoryRepository.findByCategoryCode(category.getCategoryCode());
+    }
+
+    public List<Category> fetchNextNumber(String categoryCode){
+        Category category = findByCategoryCode(categoryCode);
+        if(category!=null){
+            Category newCategory = updateByValue(category);
+            return List.of(category,newCategory);
+        }
+        else{
+            save(Category.builder().categoryCode(categoryCode).value(0L).build());
+            Category newCategory = updateByValue(category);
+            return List.of(category,newCategory);
+        }
     }
 
 }
