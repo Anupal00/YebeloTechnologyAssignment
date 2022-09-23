9f9726a465f609e9982c6bfa439e9d12d85bd4a6
Anupal00 <patra.anupal@outlook.com>
2022-09-23 11:39:32 +0530

    adding the fetchNextNumber method into controller

--- src/main/java/com/example/YebeloTechnologyAssignment/controller/CategoryController.java
+++ src/main/java/com/example/YebeloTechnologyAssignment/controller/CategoryController.java
@@ -1,10 +1,15 @@
 package com.example.YebeloTechnologyAssignment.controller;
 
+import com.example.YebeloTechnologyAssignment.model.Category;
+import com.example.YebeloTechnologyAssignment.model.DemoCategoryCode;
 import com.example.YebeloTechnologyAssignment.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/FetchNextNumber")
@@ -12,5 +17,10 @@ public class CategoryController {
 
     @Autowired
     private CategoryService categoryService;
+
+    @GetMapping()
+    public List<Category> fetchNextNumber(@RequestBody DemoCategoryCode demoCategoryCode){
+        return categoryService.fetchNextNumber(demoCategoryCode.getCategoryCode());
+    }
 
 }
