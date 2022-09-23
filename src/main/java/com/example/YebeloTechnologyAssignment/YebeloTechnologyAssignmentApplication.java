d099a443013dc467ec497158479a72884043f9b0
Anupal00 <patra.anupal@outlook.com>
2022-09-23 10:01:22 +0530

    Initial commit

---
+++ src/main/java/com/example/YebeloTechnologyAssignment/YebeloTechnologyAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.YebeloTechnologyAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YebeloTechnologyAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YebeloTechnologyAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/YebeloTechnologyAssignment/YebeloTechnologyAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.YebeloTechnologyAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YebeloTechnologyAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
