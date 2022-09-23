b6076246cfa3c563d9c3c8a887582d7616363486
Anupal00 <patra.anupal@outlook.com>
2022-09-23 11:03:50 +0530

    creating a DemoCategoryCode.java for accepting JSON form request, also in service two methods are (save and fetch) have been written

---
+++ src/main/java/com/example/YebeloTechnologyAssignment/model/DemoCategoryCode.java
@@ -0,0 +1,14 @@
+package com.example.YebeloTechnologyAssignment.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class DemoCategoryCode {
+    private String categoryCode;
+}
--- src/main/java/com/example/YebeloTechnologyAssignment/service/CategoryService.java
+++ src/main/java/com/example/YebeloTechnologyAssignment/service/CategoryService.java
@@ -1,5 +1,7 @@
 package com.example.YebeloTechnologyAssignment.service;
 
+import com.example.YebeloTechnologyAssignment.model.Category;
+import com.example.YebeloTechnologyAssignment.model.DemoCategoryCode;
 import com.example.YebeloTechnologyAssignment.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,5 +12,12 @@ public class CategoryService {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    public Category findByCategoryCode(DemoCategoryCode demoCategoryCode){
+        return categoryRepository.findByCategoryCode(demoCategoryCode.getCategoryCode());
+    }
+
+    public Category save(Category c){
+        return categoryRepository.save(c);
+    }
 
 }
